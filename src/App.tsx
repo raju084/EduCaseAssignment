6b20d12bca53ecedee35e8570df0f57b4032ad1e
raju084 <nattalabhagyaraj@gmail.com>
2026-06-06 13:30:15 +0530

    feat(core): setup application routing and entry point

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import LandingPage from './pages/LandingPage';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import './index.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="popx-container">
+          <Routes>
+            <Route path="/" element={<Navigate to="/landing" replace />} />
+            <Route path="/landing" element={<LandingPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<Navigate to="/landing" replace />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
